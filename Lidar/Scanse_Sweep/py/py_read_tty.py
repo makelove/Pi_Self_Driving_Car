cc8a75c41c59db98497dc955553bd556bb8a0abb
play4fun <play4fun@foxmail.com>
2017-09-08 10:04:06 +0800

    py_read_tty tested

--- Lidar/Scanse_Sweep/py/py_read_tty.py
+++ Lidar/Scanse_Sweep/py/py_read_tty.py
@@ -11,7 +11,8 @@ http://blog.pistuffing.co.uk/first-contact/
 
 import serial
 
-sweep = serial.Serial("/dev/ttyUSB0",
+# sweep = serial.Serial("/dev/ttyUSB0",
+sweep = serial.Serial("/dev/tty.usbserial-DM00KZW7",
                       baudrate=115200,
                       parity=serial.PARITY_NONE,
                       bytesize=serial.EIGHTBITS,
@@ -20,7 +21,7 @@ sweep = serial.Serial("/dev/ttyUSB0",
                       rtscts=False,
                       dsrdtr=False)
 print("Scanse Sweep open")
-sweep.write("ID\n")
+sweep.write(b"ID\n")
 print("Version requested")
 resp = sweep.readline()
-print("Response: " + resp)
+print("Response: ", resp)#Response:  b'ID115200110050500\n'
