e4caa2f04d0d5b3178cb8a58802399d1d6084c58
play4fun <play4fun@foxmail.com>
2017-09-08 10:00:53 +0800

    first_scan_task

---
+++ Lidar/Scanse_Sweep/py/first_scan_task.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/9/7 10:02
+# @Author  : play4fun
+# @File    : te1.py
+# @Software: PyCharm
+
+"""
+te1.py:
+"""
+
+from sweeppy import Sweep
+
+with Sweep('/dev/ttyUSB0') as sweep:
+    # with Sweep('/dev/tty.usbserial-DM00KZW7') as sweep:
+    print('start_scanning')
+    sweep.start_scanning()
+    try:
+        for scan in sweep.get_scans():
+            print('{}\n'.format(scan))
+    except KeyboardInterrupt as e:
+        print(e, 'Stop scanning')
+        sweep.stop_scanning()
+    finally:
+        sweep.set_motor_speed(0)
+# 正常工作！
+
+'''
+#run at  ipython
+from sweeppy import Sweep
+with Sweep('/dev/ttyUSB0') as sweep:
+    print('start_scanning')
+    sweep.start_scanning()
+    scan1 = sweep.get_scans()
+    scan=scan1.__next__()
+    sweep.stop_scanning()
+    sweep.set_motor_speed(0)
+
+'''
